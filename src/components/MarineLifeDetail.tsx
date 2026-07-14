3e9b5e5333e82969e19f8ffa6dd7b70154717f82
Peter Greaney <petergreaney@me.com>
2026-07-14 20:12:30 +0700

    hjjhh

--- src/components/MarineLifeDetail.tsx
+++ src/components/MarineLifeDetail.tsx
@@ -1,5 +1,5 @@
 
-mport React from 'react';
+import React from 'react';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
