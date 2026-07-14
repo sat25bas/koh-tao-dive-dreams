b1cd2f19f7e6d3cbc03c8d74ec88af048ae3bc49
Peter Greaney <petergreaney@me.com>
2026-07-14 09:09:41 +0700

    yuopdate

--- src/pages/dive-sites/sail-rock.tsx
+++ src/pages/dive-sites/sail-rock.tsx
@@ -29,7 +29,7 @@ const SailRock = () => {
         diving_tips:
           'Gevorderde certificering vereist - dit is een diepduiksite\nStromingsbewustzijn is essentieel - blijf bij je gids\nGebruik een duikcomputer voor nauwkeurige diepte- en tijdsbewaking\nWalvishaaien zijn vaak actiever in de ochtend - vroeg vertrek aanbevolen\nNeem een groothoeklens mee voor grote pelagische soorten\nSpaar lucht in verband met diepte en mogelijke stroming\nPerfect voor specialisaties in diepduiken',
         images:
-          '/images/sailrock.webp\n/images/photo-1613853250147-2f73e55c1561.avif\n/images/photo-1618865181016-a80ad83a06d3.avif\n/images/photo-1647825194145-2d94e259c745.avif\n/images/photo-1682686580849-3e7f67df4015.avif\n/images/photo-1682687982423-295485af248a.avif',
+          '/images/sailrock.webp\n/images/marine-life-hero.webp\n/images/whale.webp\n/images/photo-1682686580849-3e7f67df4015.avif\n/images/photo-1659518893171-b15e20a8e201.avif\n/images/chumphon-pinnacle-top.webp',
       }
     : {
         overview:
@@ -44,7 +44,7 @@ const SailRock = () => {
         diving_tips:
           'Advanced certification required - this is a deep dive site\nStrong current awareness essential - stay with your guide\nUse a dive computer for accurate depth and time monitoring\nWhalesharks are most active in the morning - early boat departure recommended\nBring a wide-angle lens for photographing large pelagics\nConserve air due to depth and potential current\nPerfect for deep specialty course dives',
         images:
-          '/images/sailrock.webp\n/images/photo-1613853250147-2f73e55c1561.avif\n/images/photo-1618865181016-a80ad83a06d3.avif\n/images/photo-1647825194145-2d94e259c745.avif\n/images/photo-1682686580849-3e7f67df4015.avif\n/images/photo-1682687982423-295485af248a.avif',
+          '/images/sailrock.webp\n/images/marine-life-hero.webp\n/images/whale.webp\n/images/photo-1682686580849-3e7f67df4015.avif\n/images/photo-1659518893171-b15e20a8e201.avif\n/images/chumphon-pinnacle-top.webp',
       }), [isDutch]);
 
   const { content } = usePageContent({ pageSlug: 'sail-rock', locale, fallbackContent });
