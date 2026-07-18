f4fa125181aa9a5cae3292292ea78c003f7d018d
one-media-asia <onemediaasia@duck.com>
2026-07-18 20:15:58 +0700

    Update BeachesKohTao.tsx

--- src/pages/BeachesKohTao.tsx
+++ src/pages/BeachesKohTao.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-const beaches = [
+const beaches = [//
   {
     name: "Mae Haad Beach",
     subtitle: "Noord & Zuid",
